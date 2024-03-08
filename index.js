806f0646aedc52517b5bd953ebcc22d82bde5d1f
Siddarth Koul <siddarthkoul21@gmail.com>
2024-03-08 21:01:58 +0530

    Connected Database

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
+const { default: mongoose } = require('mongoose');
 const app = express();
 dotenv.config();
 
+const connectDB = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log("DB Connected");
+    } catch (err) {
+        console.error(`Error ${err}`);
+    }
+};
+
+connectDB();
 app.get('/', (req, res) => {
     res.send("API chal rhi hai, chinta mat kar")
 })
-
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log("Server is running"));
