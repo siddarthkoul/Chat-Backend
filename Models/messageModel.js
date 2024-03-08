4d8fcacbf2f85971ab716aa5c608d462b06f8f93
Siddarth Koul <siddarthkoul21@gmail.com>
2024-03-08 17:05:41 +0530

    Created Schemas

---
+++ Models/messageModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const messageModel = mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    chat: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.Model("Message",messageModel);
+module.exports = Message;
---
+++ Models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.Model("User",userModel);
+module.exports = User;
